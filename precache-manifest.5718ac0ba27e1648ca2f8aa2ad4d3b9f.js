71fa5a69467e03b797e30bddaca0cb841b4dbaff
Agustin Vignolo <agustinvignolo@MacBook-Air-de-Agustin.local>
2020-05-30 19:32:02 -0300

    Updates

---
+++ precache-manifest.5718ac0ba27e1648ca2f8aa2ad4d3b9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9d7f4b98e72dc47467d815e58983f5b",
+    "url": "/react-pixabay/index.html"
+  },
+  {
+    "revision": "09002c18a124d6d66f01",
+    "url": "/react-pixabay/static/js/2.be7ce9bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-pixabay/static/js/2.be7ce9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde546847bf3883ab3bd",
+    "url": "/react-pixabay/static/js/main.dff26454.chunk.js"
+  },
+  {
+    "revision": "a5089d0af49082fec8ba",
+    "url": "/react-pixabay/static/js/runtime-main.a3ff0e47.js"
+  }
+]);
